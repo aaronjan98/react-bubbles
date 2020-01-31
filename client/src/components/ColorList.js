9ef0622daa96a89bdfce9250834631b7607d1f0d
aaronjan <aaronjan98@gmail.com>
2020-01-31 13:01:35 -0800

    clean uped some unnecessary imports and such

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -19,9 +19,6 @@ const ColorList = ({ colors, updateColors }) => {
 	console.log(colorToEdit);
 	const saveEdit = (e) => {
 		e.preventDefault();
-		// Make a put request to save your updated color
-		// think about where will you get the id from...
-		// where is it saved right now?
 		axiosWithAuth()
 			.put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
 			.then((res) => {
@@ -41,7 +38,6 @@ const ColorList = ({ colors, updateColors }) => {
 	console.log('colors', colors);
 
 	const deleteColor = (color) => {
-		// make a delete request to delete this color
 		console.log('delete this color', color);
 		axiosWithAuth()
 			.delete(`http://localhost:5000/api/colors/${color.id}`)
