78341a4786a5926f0412256e3e8d2ea25729babe
aaronjan <aaronjan98@gmail.com>
2020-01-31 08:35:13 -0800

    text aligned to center App in scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,35 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import Login from './components/Login';
+import BubblePage from './components/BubblePage';
+import PrivateRoute from './components/PrivateRoute';
+
+import './styles.scss';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
+	return (
+		<div className="App">
+			{/* <ul>
+				<li>
+					<Link to="/login">Login</Link>
+				</li>
+				<li>
+					<Link to="/protected">Friends List</Link>
+				</li>
+			</ul> */}
+			<Switch>
+				<PrivateRoute path="/bubble-page" component={BubblePage} />
+				{/* <PrivateRoute path="/" component={} /> */}
+				<Route path="/login" component={Login} />
+				<Route component={Login} />
+			</Switch>
+			{/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
-      </div>
-    </Router>
-  );
+		</div>
+	);
 }
 
 export default App;
